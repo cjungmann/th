3633a35465db273c05b90114725500939e3184d7
Chuck Jungmann <chuck@cpjj.net>
2020-12-12 06:25:37 -0600

    Tons of new code, functionality.

--- src/thesaurus.c
+++ src/thesaurus.c
@@ -1,16 +1,85 @@
 #include "bdb.h"
 #include "ivtable.h"
+#include "utils.h"
 #include "thesaurus.h"
 
 #include <string.h>   // for memset()
 
+void   ttabs_init_imp(TTABS *ttabs);
+Result ttabs_open_imp(TTABS *ttabs, const char *name);
+void   ttabs_close_imp(TTABS *ttabs);
+Result ttabs_add_word_imp(TTABS *ttabs, const char *str, int size, bool newline);
+Result ttabs_get_word_imp(TTABS *ttabs, RecID id, char *buffer, DataSize size);
+RecID  ttabs_lookup_imp(TTABS *ttabs, const char *str);
+Result ttabs_get_words_imp(TTABS *ttabs, RecID id, word_list_user user);
 
-int save_thesaurus_word(const char *str, int size, int newline, void *data)
+
+void i2i_opener(const char *path, void *data);
+Result open_i2i(DB **db, int create, const char *name, const char *ext);
+bool save_word_links(TTABS *ttabs, RecID root, RecID word);
+
+TTABS_Class TTB = {
+   ttabs_init_imp,
+   ttabs_open_imp,
+   ttabs_close_imp,
+   ttabs_add_word_imp,
+   ttabs_get_word_imp,
+   ttabs_lookup_imp,
+   ttabs_get_words_imp
+};
+
+void ttabs_init_imp(TTABS *ttabs)
 {
+   init_IVTable(&ttabs->ivt);
+   ttabs->db_r2w = NULL;
+   ttabs->db_w2r = NULL;
+}
+
+Result ttabs_open_imp(TTABS *ttabs, const char *name)
+{
+   DB *db_r2w, *db_w2r;
+   Result result;
+   IVTable *ivt = &ttabs->ivt;
+   if ((result = ivt->open(ivt, name)))
+      goto abandon_function;
+
+   if ((result = open_i2i(&db_r2w, 1, name, "r2w")))
+      goto abandon_ivt;
+
+   if ((result = open_i2i(&db_w2r, 1, name, "w2r")))
+      goto abandon_r2w;
+
+   ttabs->db_r2w = db_r2w;
+   ttabs->db_w2r = db_w2r;
+   goto abandon_function;
+
+  abandon_r2w:
+   db_r2w->close(db_r2w, 0);
+
+  abandon_ivt:
+   ivt->close(ivt);
+
+  abandon_function:
+   return result;
+}
+
+void ttabs_close_imp(TTABS *ttabs)
+{
+   ttabs->db_r2w->close(ttabs->db_r2w, 0);
+   ttabs->db_w2r->close(ttabs->db_w2r, 0);
+   ttabs->ivt.close(&ttabs->ivt);
+
+   memset(ttabs, 0, sizeof(TTABS));
+}
+
+Result ttabs_add_word_imp(TTABS *ttabs, const char *str, int size, bool newline)
+{
+   static RecID rootid = 0;
+
    Result result;
    DB *db = NULL;    // in case we need to report an error
 
-   IVTable *ivt = (IVTable*)data;
+   IVTable *ivt = &ttabs->ivt;
 
    DBT key;
    set_dbt(&key, (void*)str, size);
@@ -75,24 +144,273 @@ int save_thesaurus_word(const char *str, int size, int newline, void *data)
 
    // Errors will have exited by now.
    // Update inter-word indexes
-   if (!result)
+   if (recid)
    {
       if (newline)
-      {
-         // Update static root-word recid,
-         // but wait to link following words.
-         ;
-      }
+         rootid = recid;
       else
-      {
-         // Using the static root-word recid, 
-         // Add root-to-word link
-         // Add word-to-root link
-         ;
-      }
+         save_word_links(ttabs, rootid, recid);
+         /* rootid = rootid; */
    }
 
    return 1;
+}
+
+Result ttabs_get_word_imp(TTABS *ttabs, RecID id, char *buffer, DataSize size)
+{
+   DBT value;
+   memset(&value, 0, sizeof(DBT));
+
+   Result result = ttabs->ivt.get_record_by_recid(&ttabs->ivt, id, &value);
+   if (result == 0)
+   {
+      if (value.size < size)
+      {
+         memcpy(buffer, value.data, value.size);
+         buffer[value.size] = '\0';
+      }
+      else
+         result = DB_BUFFER_SMALL;
+   }
+
+   return result;
+}
+
+RecID ttabs_lookup_imp(TTABS *ttabs, const char *str)
+{
+   IVTable *ivt = &ttabs->ivt;
+   return ivt->get_recid(ivt, (void*)str, strlen(str));
+}
+
+
+/****
+ * Beginning of get_words() section
+ * The next section (typedef + get_words_closure() + get_words_callback() + ttabs_get_word_list_imp()
+ * work together, with callbacks, closure struct.
+ */
+typedef bool (*cursor_callback)(DBT *key, DBT *value, void *closure);
+
+void run_cursor_with_closure(DBC *cursor, DBT *key, cursor_callback cb, void *closure)
+{
+   Result result;
+   DBT value;
+   memset(&value, 0, sizeof(DBT));
+
+   if (!(result = cursor->get(cursor, key, &value, DB_SET)))
+   {
+      while ((*cb)(key, &value, closure))
+         cursor->get(cursor, key, &value, DB_NEXT);
+   }
+}
+
+typedef struct get_words_closure {
+   RecID id;
+   int   count;
+   RecID *list;
+} GWC;
+
+/** This is used as a *cursor_callback function pointer */
+bool get_words_callback(DBT *key, DBT *value, void *closure)
+{
+   GWC *gwc = (GWC*)closure;
+
+   if (*(RecID*)key->data == gwc->id)
+   {
+      if (gwc->list)
+         *gwc->list++ = *(RecID*)value->data;
+      else
+         ++gwc->count;
+
+      return 1;
+   }
+   else
+      return 0;
+}
+
+Result ttabs_get_words_imp(TTABS *ttabs, RecID id, word_list_user user)
+{
+   DB *db = ttabs->db_r2w;
+   GWC gwc;
+   memset(&gwc, 0, sizeof(GWC));
+   gwc.id = id;
+
+
+   DBC *cursor;
+   Result result;
+
+   if (!(result = db->cursor(db, NULL, &cursor, 0)))
+   {
+      DBT key;
+      set_dbt(&key, &id, sizeof(RecID));
+      run_cursor_with_closure(cursor, &key, get_words_callback, &gwc);
+      if (gwc.count)
+      {
+         RecID idlist[gwc.count];
+         gwc.list = idlist;
+         memset(&idlist, 0, sizeof(idlist));
+
+         // The key must reset to rerun the list
+         set_dbt(&key, &id, sizeof(RecID));
+         run_cursor_with_closure(cursor, &key, get_words_callback, &gwc);
+         (*user)(idlist, &idlist[gwc.count], ttabs);
+      }
+
+      cursor->close(cursor);
+   }
+
+   return result;
+}
+
+/******
+ * End of get_words section
+ */
+
+Result saved_ttabs_get_words_imp(TTABS *ttabs, RecID id, word_list_user user)
+{
+   DB *db = ttabs->db_r2w;
+   int count = 0;
+   DBT key;
+   set_dbt(&key, &id, sizeof(RecID));
+   DBT value;
+   memset(&value, 0, sizeof(RecID));
+
+   DBC *cursor;
+   Result result;
+
+   if ((result = db->cursor(db, NULL, &cursor, 0)))
+      goto abandon_function;
+
+   if ((result = cursor->get(cursor, &key, &value, DB_SET)))
+      goto abandon_cursor;
+
+   while (*(RecID*)key.data == id)
+   {
+      ++count;
+
+      if ((result = cursor->get(cursor, &key, &value, DB_NEXT)))
+         break;
+   }
+
+
+  abandon_cursor:
+   cursor->close(cursor);
+
+  abandon_function:
+   return result;
+}
+                       
+
+
+
+typedef struct _db_opening {
+   DB     *db;
+   Result result;
+   bool   create;
+} DBO;
+
+// Callback function for strargs_builder when called by open_i2i
+void i2i_opener(const char *path, void *data)
+{
+   DBO *dbo = (DBO*)data;
+
+   DB *db;
+   Result *result = &dbo->result;
+
+   if ((*result = db_create(&db, NULL, 0)))
+      goto abandon_function;
+
+   if ((*result = db->set_flags(db, DB_DUPSORT)))
+      goto abandon_db;
+
+   if ((*result = db->open(db, NULL, path, NULL, DB_BTREE, DB_CREATE | DB_OVERWRITE, 0664)))
+      goto abandon_db;
+
+   dbo->db = db;
+   goto abandon_function;
+
+  abandon_db:
+   db->close(db, 0);
+
+  abandon_function:
+   ;
+}
+
+Result open_i2i(DB **db, int create, const char *name, const char *ext)
+{
+   DBO dbo;
+   memset(&dbo, 0, sizeof(DBO));
+
+   dbo.create = create;
+
+   strargs_builder(i2i_opener, &dbo, name, ".", ext, NULL);
+
+   if (dbo.result==0)
+      *db = dbo.db;
+
+   return dbo.result;
+}
+
+bool save_word_links_inkey(TTABS *ttabs, RecID root, RecID word)
+{
+   RecID arr_r2w[2] = { root, word };
+   DBT r2w;
+   set_dbt(&r2w, arr_r2w, sizeof(arr_r2w));
+
+   RecID arr_w2r[2] = { word, root };
+   DBT w2r;
+   set_dbt(&w2r, arr_w2r, sizeof(arr_w2r));
+
+   DBT value;
+   memset(&value, 0, sizeof(DBT));
+
+   DB *db;
+   Result result;
+   
+   db = ttabs->db_r2w;
+   if ((result = db->put(db, NULL, &r2w, &value, DB_NODUPDATA)))
+      if (result != DB_KEYEXIST)
+         db->err(db, result, "Making root-to-word (%u -> %u) link.", root, word);
+
+   db = ttabs->db_w2r;
+   if ((result = db->put(db, NULL, &w2r, &value, DB_NODUPDATA)))
+      if (result != DB_KEYEXIST)
+         db->err(db, result, "Making word-to-root (%u -> %u) link.", word, root);
+
+   return 1;
+}
+
+bool save_word_links(TTABS *ttabs, RecID root, RecID word)
+{
+   DBT dbt_root;
+   set_dbt(&dbt_root, &root, sizeof(RecID));
+   DBT dbt_word;
+   set_dbt(&dbt_word, &word, sizeof(RecID));
+
+   DB     *db;
+   Result result;
+
+   db = ttabs->db_r2w;
+   if ((result = db->put(db, NULL, &dbt_root, &dbt_word, DB_NODUPDATA)))
+   {
+      if (result != DB_KEYEXIST)
+         db->err(db, result, "Making root-to-word (%u -> %u) link.", root, word);
+   }
+
+   db = ttabs->db_w2r;
+   if ((result = db->put(db, NULL, &dbt_word, &dbt_root, DB_NODUPDATA)))
+   {
+      if (result != DB_KEYEXIST)
+         db->err(db, result, "Making word-to-root (%u -> %u) link.", word, root);
+   }
+
+   return 1;
+}
+
+
+bool save_thesaurus_word(const char *str, int size, bool newline, void *data)
+{
+   TTABS *ttabs = (TTABS*)data;
+   return TTB.add_word(ttabs, str, size, newline);
 }
 
 bool thesaurus_dumpster(DBT *key, DBT *value, void *data)
@@ -100,6 +418,47 @@ bool thesaurus_dumpster(DBT *key, DBT *value, void *data)
    TREC *trec = (TREC*)value->data;
    int word_size = value->size - sizeof(TREC);
    const char *color = trec->is_root ? "\x1b[32;1m" : "";
-   printf("%s%5u %.*s\x1b[m\n", color, trec->count, word_size, trec->value);
+   printf("%s%7u %5u %.*s\x1b[m\n",
+          color,
+          *(RecID*)key->data,
+          trec->count,
+          word_size, trec->value);
    return 1;
 }
+
+#ifdef THESAURUS_MAIN
+
+#include <stdio.h>
+#include "bdb.c"
+#include "ivtable.c"
+#include "utils.c"
+
+void test_TTABS_opener(void)
+{
+   const char *tname = "ttabs_test";
+   Result result;
+   TTABS ttabs;
+   TTB.init(&ttabs);
+
+   if ((result = TTB.open(&ttabs, tname)))
+      printf("Failed to build \"%s\".\n", tname);
+   else
+      printf("Successfully created \"%s\".\n", tname);
+}
+
+int main(int argc, const char **argv)
+{
+   test_TTABS_opener();
+
+   return 0;
+}
+#endif
+
+
+/* Local Variables: */
+/* compile-command: "b=thesaurus; \*/
+/*  cc -Wall -Werror -ggdb        \*/
+/*  -std=c99 -pedantic            \*/
+/*  -ldb                          \*/
+/*  -D${b^^}_MAIN -o $b ${b}.c"   \*/
+/* End: */
--- src/thesaurus.h
+++ src/thesaurus.h
@@ -9,9 +9,39 @@ typedef struct _tword_head {
    char     value[];
 } TREC;
 
+typedef struct _thesaurus_tables {
+   IVTable ivt;
+   DB      *db_r2w;
+   DB      *db_w2r;
+} TTABS;
 
-int save_thesaurus_word(const char *str, int size, int newline, void *data);
+typedef void (*word_list_user)(RecID *list, RecID *end, void *closure);
+
+typedef void   (*ttabs_init)     (TTABS *ttabs);
+typedef Result (*ttabs_open)     (TTABS *ttabs, const char *name);
+typedef void   (*ttabs_close)    (TTABS *ttabs);
+typedef Result (*ttabs_add_word) (TTABS *ttabs, const char *str, int size, bool newline);
+typedef Result (*ttabs_get_word) (TTABS *ttabs, RecID id, char *buffer, DataSize size);
+typedef RecID  (*ttabs_lookup)   (TTABS *ttabs, const char *str);
+typedef Result (*ttabs_get_words)(TTABS *ttabs, RecID id, word_list_user user);
+
+bool save_thesaurus_word(const char *str, int size, bool newline, void *data);
 bool thesaurus_dumpster(DBT* key, DBT* value, void *data);
+
+typedef struct ttabs_class {
+   ttabs_init      init;
+   ttabs_open      open;
+   ttabs_close     close;
+   ttabs_add_word  add_word;
+   ttabs_get_word  get_word;
+   ttabs_lookup    lookup;
+   ttabs_get_words get_words;
+} TTABS_Class;
+
+extern TTABS_Class TTB;
+
+
+
 
 
 
