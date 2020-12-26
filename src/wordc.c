b396aed9f7bfc3982a7052b6412b231c8b77d8f1
Chuck Jungmann <chuck@cpjj.net>
2020-12-26 15:54:43 -0600

    Added several options for testing.

--- src/wordc.c
+++ src/wordc.c
@@ -69,47 +69,99 @@ Result wcc_add_word(WCC *wcc, const char *line, const char *end)
 
 #ifdef WORDC_MAIN
 
+#include <readargs.h>
+
 #include "bdb.c"
 #include "utils.c"
 #include "read_file_lines.c"
 
-void test_open(void)
+const char *import_file = "../files/count_1w.txt";
+int        add_number_commas = 0;
+int        flag_raw_output = 0;
+int        min_word_len = 0;
+
+raAction actions[] = {
+   {'h', "help", "This help display", &ra_show_help_agent},
+   {'b', "buffer", "Buffer size", &ra_int_agent, &RFL_bufflen },
+   {'i', "import", "Import file path", &ra_string_agent, &import_file },
+   {'c', "commaize", "Add commas to large numbers", &ra_flag_agent, &add_number_commas },
+   {'r', "raw-mode", "Output same input", &ra_flag_agent, &flag_raw_output },
+   {'w', "width", "Minimum word length", &ra_int_agent, &min_word_len },
+   {'s', "show", "Show settings", &ra_show_values_agent}
+};
+
+
+void print_word(const char *start, const char *end, int min_width)
 {
-   Result result;
-   WCC wcc;
-   memset(&wcc, 0, sizeof(WCC));
-   if (!(result = wcc_open(&wcc, "wordc.db", 1)))
-   {
-      printf("Created the database.\n");
-   }
-   else
-      printf("Failed to create the database.\n");
+   int word_len = end - start;
+
+   printf("%.*s", word_len, start);
+
+   if (word_len < min_width)
+      printf("%*.s", (int)(min_width - word_len), "");
 }
 
-bool read_file_lines_user(const char *start, const char *end, void *closure)
+/**
+ * tweak output for readability, adding commas to numbers, and
+ * optionally through the -w option, adding spaces to words to
+ * align the numbers column.
+ */
+bool rfl_commaize_user(const char *start, const char *end, void *closure)
 {
    const char *end_word;
    uint64_t   value;
    interpret_string_number(start, end, &end_word, &value);
 
-   printf("\"%.*s\"  ", (int)(end_word - start), start);
+   print_word(start, end_word, min_word_len);
+   fputc(' ', stdout);
    commaize_number(value);
-   printf(" times.\n");
+   fputc('\n', stdout);
 
    return 1;
 }
 
-void test_read_file_lines(void)
+/**
+ * For the word count list, this output should be the same
+ * as the raw user, but this function parses the line into
+ * a word and a number before printing, so it tests both the
+ * line-reading ability, but also the line parsing ability.
+ */
+bool rfl_comparable_user(const char *start, const char *end, void *closure)
 {
-   const char *source = "../files/count_1w2.txt";
-   /* const char *source = "wordc.txt"; */
-   read_file_lines(source, read_file_lines_user, NULL);
+   const char *end_word;
+   uint64_t   value;
+   interpret_string_number(start, end, &end_word, &value);
+
+   printf("%.*s\t%lu\n", (int)(end_word - start), start, value);
+
+   return 1;
+}
+
+/**
+ * Echo out each line, exactly as it was read.
+ */
+bool rfl_raw_user(const char *start, const char *end, void *closure)
+{
+   printf("%.*s\n", (int)(end-start), start);
+   return 1;
 }
 
 int main(int argc, const char **argv)
 {
-   /* test_open(); */
-   test_read_file_lines();
+   ra_set_scene(argv, argc, actions, ACTS_COUNT(actions));
+
+   if (ra_process_arguments())
+   {
+      line_user_f line_user = rfl_comparable_user;
+      if (flag_raw_output)
+         line_user = rfl_raw_user;
+      else if (add_number_commas)
+         line_user = rfl_commaize_user;
+
+      int result = read_file_lines(import_file, line_user, NULL);
+      if (result)
+         printf("There was a failure of read_file_lines (%s).\n", db_strerror(result));
+   }
 
    return 0;
 }
