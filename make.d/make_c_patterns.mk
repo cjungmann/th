bd8a95611122b763b7f015b29939a12f6086a9da
Chuck Jungmann <chuck@cpjj.net>
2021-03-18 17:14:38 -0500

    Updated from fixed file from makefile_patterns.git

--- make.d/make_c_patterns.mk
+++ make.d/make_c_patterns.mk
@@ -6,7 +6,8 @@
 # its instructive features.  The USAGE particularly informs
 # usage of the specific repository.
 # USAGE:
-# 1. Copy this makefile where your main makefile can include it.
+# 1. Include CP_PREPARE_SOURCES before the main target in your
+#    default rule.
 # 2. Define a variable with a list of desired module names,
 #    not including the .c or .h extensions.
 # 3. Put the *include make_c_patterns.mk* statement
@@ -17,28 +18,49 @@
 # 5. For cleaning targets, you can use ${CP_SOURCES} and ${CP_HEADERS}
 #    to remove links to .c and .h files, respectively.
 #
-# Usage Example makefile fragment, assuming MODULES contains objects to link:
+# Example:
 #
 # MODULES != ls -1 ${SRC}/*.c | sed 's/\.c/.o/g'
-# ...
-# CP_NAMES = get_key-ress prompter columnize
-# include make.d/make_c_patterns.mk
+#
+# all: CP_PREPARE_SOURCES ${TARGET}
+#
+# CP_NAMES = get_keypress prompter columnize
+# include make_c_patterns.mk
 # MODULES += ${CP_OBJECTS}
+#
+# ${TARGET}: ${MODULES}
+#	linking-recipe
 
-
+# Ignoring $(CP_NAMES:=.o) in favor of using != for BSD portability
 CP_SOURCES != echo ${CP_NAMES} | sed -E 's/([^[:space:]]+)/${SRC}\/\1.c/g'
 CP_HEADERS != echo ${CP_SOURCES} | sed -E 's/\.c/\.h/g'
 CP_OBJECTS != echo ${CP_SOURCES} | sed -E 's/\.c/\.o/g'
 
-CP_LN_RECIPE != echo ${CP_NAMES} | sed -E 's/([^[:space:]]+)/ln -sf c_patterns\/\1.c ${SRC}\/\1.c\\;/g'
+# Detect if links must be established:
+CP_LINKS_REGEX != echo ${CP_NAMES} | sed -E 's/([^[:space:]]+)/\1.o/g' | sed -E 's/[[:space:]]+/\\|/g'
+CP_LINKS_FOUND != ls -1 ${SRC} | grep -E \(${CP_LINKS_REGEX}\)
+CP_NAMES_COUNT != echo ${CP_NAMES} | wc -w
+CP_SOURCES_COUNT != ls -1 ${CP_OBJECTS} 2>/dev/null | wc -l
+CP_SOURCE_LINKS_NEEDED != n=${CP_NAMES_COUNT}; s=${CP_SOURCES_COUNT}; \
+	if [ $$n -gt $$s ]; then echo 1; else echo 0; fi;
 
-${CP_SOURCES}: c_patterns
-	@echo Making link from c_patterns to $@ for $(*F)
-	ln -fs ${PWD}/c_patterns/$(*F).c ${PWD}/${SRC}/$(*F).c
-	ln -fs ${PWD}/c_patterns/$(*F).h ${PWD}/${SRC}/$(*F).h
+# This variable will contain a list of names for making links
+# if the links are not detected.  If not empty, it will be a
+# list of artificial prerequisite that will trigger the links.
+CP_LINK_PREREQS != t=${CP_SOURCE_LINKS_NEEDED}; if [ $$t -eq 1 ]; then echo ${CP_NAMES}; fi; [ 1 -eq 1 ]
+
+# Initiate the link-making
+CP_PREPARE_SOURCES: ${CP_LINK_PREREQS}
+
+${CP_NAMES}: c_patterns
+	@echo Making link from c_patterns to $@
+	ln -fs ${PWD}/c_patterns/$@.c ${PWD}/${SRC}/$@.c
+	ln -fs ${PWD}/c_patterns/$@.h ${PWD}/${SRC}/$@.h
+
+pull_c_patterns: c_patterns
+	git -C c_patterns pull origin
 
 c_patterns:
 	@echo "Clone c_patterns project and link requested files to src directory."
 	git clone http://www.github.com/cjungmann/c_patterns.git
 
-Confirm_C_Patterns: ${CP_SOURCES}
