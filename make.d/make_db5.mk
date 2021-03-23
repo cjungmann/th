8ab971e8f2b3c9bb052454111b0707116b44a837
Chuck Jungmann <chuck@cpjj.net>
2021-03-23 15:13:45 -0500

    Update with newer version from makefile_patterns.git

--- make.d/make_db5.mk
+++ make.d/make_db5.mk
@@ -10,49 +10,52 @@
 #    This will generate an error and exit if libdb-5.so is not
 #    available.  Omit this step if you want to ignore a missing
 #    library.
-# 3. Add ${DB5_INC} to module-building rules to ensure that the
-#    compiler uses the correct header.  This can be done by
-#    appending ${DB5_INC} to CFLAGS, or to add ${DB5_INC} directly
-#    into the recipe.
-# 4. Add ${DB5_LINK} to the linker recipe.
+# 3. Use output variables DB5_INC and DB5_LINK for compiling and
+#    linking, respectively.  Generally, you'll concatenate each
+#    to its respective default values, CFLAGS and LDLIBS
 #
 # Example:
 #
+# Initialize implicit rule default values:
 # CFLAGS = -Wall -Werror -pedantic -ggdb
+# LDLIBS = 
+#
 # all: Confirm_DB5 ${TARGET}
 #
 # include make_db5.mk
 # CFLAGS += ${DB5_INC}
+# LDLIBS += ${DB5_LINK}
 #
 # ${TARGET}: ${MODULES}
 #	${CC} -o $@ $? ${DB5_LINK}
 # 
 
+# The first set of variables are used to query
+# the environment, setting flags for "output"
+# variables DB5_INC and DB5_LINK:
 DB_INSTALLED != find /usr -name db.h 2>/dev/null | grep /include/db.h
 DB_IS_DB5 != a=${DB_INSTALLED}; if grep -q DB_VERSION_MAJOR "$$a"; then echo 1; else echo 0; fi; [ 1 -eq 1 ]
 DB5_ALTERNATIVE != find /usr -name db.h 2>/dev/null | grep /include/db5/db.h
 DB5_ALTERNATIVE_PATH != d=${DB5_ALTERNATIVE}; echo "$${d%/*}"
 DB5_USE_ALTERNATIVE != a=${DB5_ALTERNATIVE}; if [ -f "$$a" ]; then echo 1; else echo 0; fi; [ 1 -eq 1 ];
+DB5_ALTERNATIVE_LIB != find /usr -name libdb.so 2>/dev/null | grep "lib/db5"; [ 1 -eq 1 ]
 
 DB5_NEEDED != d=${DB_IS_DB5}; d5=${DB5_USE_ALTERNATIVE}; if [ $$d -eq 0 ] && [ $$d5 -eq 0 ]; then echo 1; else echo 0; fi; [ 1 -eq 1 ]
 
+# Feedback variable with contain a command if the builder
+# needs to take some action, or empty if everything is good.
 DB5_MESSAGE != d=${DB5_NEEDED}; if [ $$d -eq 1 ]; then echo "@echo \\\"Use your package manager to install version 5 of the Berkeley Database.\\\"; exit 1"; else echo "@echo Appropriate version of Berkeley Database found."; fi; [ 1 -eq 1 ]
 
+# Build output variable
 DB5_INC != d=${DB_IS_DB5}; dp=${DB5_ALTERNATIVE_PATH}; if [ $$d -eq 0 ] && [ $$dp ]; then echo "-I$$dp"; fi;
 
-DB5_ALTERNATIVE_LIB != find /usr -name libdb.so 2>/dev/null | grep "lib/db5"; [ 1 -eq 1 ]
+# Build output variable
 DB5_LINK != du=${DB5_USE_ALTERNATIVE}; dal=${DB5_ALTERNATIVE_LIB}; if [ $$du -eq 1 ] && [ "$$dal" ]; then echo "-L$${dal%/*}"; fi;
 DB5_LINK += -ldb
 
-
+# This rule, if included as a prerequisite in the default rule,
+# will display a message if necessary.
+.PHONY: Confirm_DB5
 Confirm_DB5:
 	${DB5_MESSAGE}
 
-DB5_debug:
-	@echo DB_INSTALLED is ${DB_INSTALLED}
-	@echo DB_IS_DB5 is ${DB_IS_DB5}
-	@echo DB5_ALTERNATIVE is ${DB5_ALTERNATIVE}
-	@echo DB5_ALTERNATIVE_PATH is ${DB5_ALTERNATIVE_PATH}
-	@echo DB5_NEEDED is ${DB5_NEEDED}
-	@echo DB5_INC is ${DB5_INC}
-	@echo DB5_LINK IS ${DB5_LINK}
